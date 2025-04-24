8a61f453e782785f9b63b03efec84b9e10fad9c3
Ayaz Panar <ayazpanar0786@gmail.com>
2025-04-24 20:18:31 +0530

    changed resume

--- src/Components/Skils/Skils.jsx
+++ src/Components/Skils/Skils.jsx
@@ -1,5 +1,12 @@
 /* eslint-disable react/prop-types */
-import { FaHtml5, FaCss3, FaJs, FaReact, FaNode, FaSchool } from "react-icons/fa";
+import {
+  FaHtml5,
+  FaCss3,
+  FaJs,
+  FaReact,
+  FaNode,
+  FaSchool,
+} from "react-icons/fa";
 import { RiNextjsFill } from "react-icons/ri";
 import { FaGraduationCap as FaCollage } from "react-icons/fa6";
 import {
@@ -12,6 +19,7 @@ import {
   SiGithub,
   SiGit,
   SiJest,
+  SiTypescript,
 } from "react-icons/si";
 import { VscVscode } from "react-icons/vsc";
 
@@ -20,7 +28,11 @@ const Skils = ({ theme }) => {
     { icon: <FaHtml5 color="#E34F26" size={50} />, label: "HTML" },
     { icon: <FaCss3 color="#1572b6" size={50} />, label: "CSS" },
     { icon: <FaJs color="yellow" size={50} />, label: "JavaScript" },
-{ icon: <RiNextjsFill color={ theme?"#000000":"#ffffff"} size={50} />, label: "Next.js" },
+    {
+      icon: <RiNextjsFill color={theme ? "#000000" : "#ffffff"} size={50} />,
+      label: "Next.js",
+    },
+    { icon: <SiTypescript color="#007ACC" size={50} />, label: "TypeScript" },
     { icon: <FaReact color="#61DBFB" size={50} />, label: "React" },
     { icon: <FaNode color="green" size={50} />, label: "Node" },
     { icon: <SiExpress color="yellow" size={50} />, label: "Express" },
@@ -29,30 +41,33 @@ const Skils = ({ theme }) => {
     { icon: <SiBootstrap color="purple" size={50} />, label: "Bootstrap" },
     { icon: <SiTailwindcss color="#38B2AC" size={50} />, label: "Tailwind" },
     { icon: <SiJquery color="#0769AD" size={50} />, label: "jQuery" },
-{ icon: <SiJest color="#99415B" size={50} />, label: "Jest" },
+    { icon: <SiJest color="#99415B" size={50} />, label: "Jest" },
     { icon: <SiGit color="orange" size={50} />, label: "Git" },
-    { icon: <SiGithub color={ theme?"#000000":"#ffffff"} size={50} />, label: "GitHub" },
+    {
+      icon: <SiGithub color={theme ? "#000000" : "#ffffff"} size={50} />,
+      label: "GitHub",
+    },
     { icon: <VscVscode color="#007ACC" size={50} />, label: "VSCode" },
   ];
 
   const education = [
     {
-      icon: <FaSchool size={40} color={`${ theme?"black":"white"}`} />,
+      icon: <FaSchool size={40} color={`${theme ? "black" : "white"}`} />,
       title: "SSC - The New Era High School, Patan",
       details: "Percentage: 54%",
     },
     {
-      icon: <FaSchool size={40} color={`${ theme?"black":"white"}`} />,
+      icon: <FaSchool size={40} color={`${theme ? "black" : "white"}`} />,
       title: "HSC - Aashish Vidhyalaya, Patan",
       details: "Percentage: 73%",
     },
     {
-      icon: <FaCollage size={40} color={`${ theme?"black":"white"}`} />,
+      icon: <FaCollage size={40} color={`${theme ? "black" : "white"}`} />,
       title: "BSc (CA & IT) - Hemchandracharya North Gujarat University, Patan",
       details: "CGPA: 7.8",
     },
     {
-      icon: <FaCollage size={40} color={`${ theme?"black":"white"}`} />,
+      icon: <FaCollage size={40} color={`${theme ? "black" : "white"}`} />,
       title: "MSc (CA & IT) - Hemchandracharya North Gujarat University, Patan",
       details: "Pursuing",
     },
